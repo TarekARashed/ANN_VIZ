27f9674e894620d5f455a63ee3ad72c3be6ad082
Tarek Rashed <98634840+TarekARashed@users.noreply.github.com>
2022-09-29 19:29:24 -0600

    Update Example3.py

--- Example3.py
+++ Example3.py
@@ -5,5 +5,5 @@ model.Add_Layer(6, "Relu")
 model.Add_Layer(2, "Relu")
 model.Add_Layer(2, "Sigmoid", Threshold_Value=0.5)
 Sample_Data=[200,100,121,88,77,99]
-model.compile(Inputs=len(Sample_Data), Random_Values=[0,1])
+model.compile(Inputs=len(Sample_Data), Random_Values=[[0,1],[-10, 10]])
 model.Create_JSON_Structure("ANN.json")
